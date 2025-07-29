1f2e8d7604dbe032585fb6d15e5c1d9e488465ee
NischalKeshav <keshavn@bentonvillek12.org>
2025-07-28 22:04:49 -0500

    Initial commit of existing project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
-
 import * as Sentry from '@sentry/react';
 
 const App = () => {
   return (
     <main className="bg-black">
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
+      <h1 class="text-white flex justify-center items-center h-screen">Hello world</h1>
     </main>
   )
 }
